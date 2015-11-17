fe4260702fec0be0d3d1ebccd8d84c447d79944d
Brad Jones <Homelesslax24@gmail.com>
2015-11-17 06:58:25 -0500

    initial commiitial commit.  copy from ALC page

---
+++ app/controllers/contacts_controller.rb
@@ -0,0 +1,17 @@
+class ContactsController < ApplicationController
+  def new
+    @contact = Contact.new
+  end
+
+  def create
+    @contact = Contact.new(params[:contact])
+    @contact.request = request
+    if @contact.deliver
+      render root_path
+      flash.now[:notice] = 'Thank you for your message. We will contact you soon!'
+    else
+      flash.now[:error] = 'Cannot send message.'
+      render root_path
+    end
+  end
+end
