1b317f04f5487cc92a5c85b543e7b094173a2074
Brad Jones <Homelesslax24@gmail.com>
2016-01-15 16:16:40 -0500

    Added admin_user login functionality

--- app/admin/admin_user.rb
+++ app/admin/admin_user.rb
@@ -1,5 +1,6 @@
 ActiveAdmin.register AdminUser do
 
+
   index do
     column :email
     column :current_sign_in_at
@@ -8,16 +9,40 @@ ActiveAdmin.register AdminUser do
     actions
   end
 
+  # show do
+  #   column :id
+  #   column :email
+  #   column :sign_in_count
+  #   column :current_sign_in_at
+  #   column :last_sign_in_at
+  #   column :current_sign_in_ip
+  #   column :last_sign_in_ip
+  #   column :created_at
+  #   column :updated_at
+  #   column :confirmed_at
+  #   column :onfirmation_sent_at
+  # end
+
+
   form do |f|
     f.inputs "Admin Details" do
       f.input :email
+      f.input :password
+      f.input :password_confirmation
     end
     f.actions
   end
 
+  filter :email 
+  filter :current_sign_in_at
+  filter :last_sign_in_at
+  filter :created_at
+  filter :updated_at
+  filter :confirmed_at
+  
 # See permitted parameters documentation:
 # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
 #
-	permit_params :email
+	permit_params :email, :password, :password_confirmation
 	
 end
