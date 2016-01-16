3addb323802acca2bda7c323bcb67325d34c69ef
Brad Jones <Homelesslax24@gmail.com>
2016-01-15 22:16:56 -0500

    Added title and meta to all page views and added hooks to the application layout

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,10 @@
 module ApplicationHelper
+	def title(page_title)
+		content_for(:title) { page_title }
+	end
+
+	# def meta_description(meta_tag)
+	# 	content_for(:meta_description) { meta_tag }
+	# end
+
 end
